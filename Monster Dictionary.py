c08e4d7be9f10652c5e510702fe2197b9cc056e5
OliverNield <206021@gardenschool.edu.my>
2022-09-09 12:20:32 +0800

    Simplified and annotated monster dictionary

--- Monster Dictionary.py
+++ Monster Dictionary.py
@@ -1,11 +1,8 @@
-import csv
-dictionary = {}
-filein = open('/Users/ramboozle/Library/Mobile Documents/com~apple~CloudDocs/Python/School Work/Things used for code/Monsters.csv','r')
-reader = csv.reader(filein)
-reader.__next__()
-for row in reader:
-    id = row[0]
-    name = row[1]
-    dictionary[id] = name
-userin = input('please input the id of the beast you wish to look up')
-print(dictionary[userin])
+import csv # imports the CVS library
+dictionary = {} #creates an empty dictionary 
+filein = open('/Users/ramboozle/Library/Mobile Documents/com~apple~CloudDocs/Python/School Work/Things used for code/Monsters.csv','r') #opens the CSV file in read mode
+reader = csv.reader(filein) #creates an object that will read every line for the loop  
+for row in reader: #repeats the code below for the amount of rows (845 times in this case)
+    dictionary[row[0]] = row[1] #stores whatever is in row 0 and 1 in the dictionary respecitivly
+userin = input('please input the id of the beast you wish to look up') #takes an input from the user as to the id to lookup
+print(dictionary[userin]) #prints the dictinary item in question 
