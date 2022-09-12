b75308a2b2813f29fd48fac93e7cecb45cba717a
OliverNield <206021@gardenschool.edu.my>
2022-09-12 11:23:57 +0800

    Completed AS01 Bubble sort
    finished an old task that needed doing

---
+++ AS01 Bubble Sort.py
@@ -0,0 +1,32 @@
+#AS01 Bubble Sort
+numbers = []
+sorted = 0
+temp1 = 0
+temp2 = 0
+temptemp = 0
+pos = 0
+sorts = 0
+numamount = int(input('how many numbers do you have?'))
+for i in range(numamount):
+    print('please input number', i+1)
+    numin = input() #accepts an input from the user
+    numbers.append(numin) #adds the inputted number to the end of the list
+#print(numbers)
+while sorted == 0:
+    for x in range(numamount-1):
+        for i in range(numamount-1):
+            #print('pos',pos)
+            temp1 = numbers[pos]
+            temp2 = numbers[pos+1]
+            #print('temp 1 & 2: ',temp1,temp2)
+            if temp1 > temp2:
+                temptemp = temp1
+                temp1 = temp2
+                temp2 = temptemp
+                #print(temp1,temp2,temptemp)
+                numbers[pos] = temp1
+                numbers[pos+1] = temp2
+            pos = pos+1
+        pos = 0
+    sorted = 1
+    print('numbers after sort',numbers)
