5fd10a39c253334f076a0612078a8304b0fc9504
OliverNield <206021@gardenschool.edu.my>
2022-10-28 11:58:47 +0800

    Did hangman

--- Current Challenge/main.py
+++ Current Challenge/main.py
@@ -1,13 +1,11 @@
-#TU20
-file = '/Users/ramboozle/Library/Mobile Documents/com~apple~CloudDocs/Coding/School Work/Things used for code/Read, Write & append/counter.txt'
+def fibseq(x):
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    return fibseq(x-1) + fibseq(x-2)
+#print(fibseq(int(input())))
 
-with open(file, "r") as existing_file:
-    for line in existing_file:
-        for word in line.split():
-            lastnum = int(word)
-
-with open(file,"a") as existing_file:
-    lastnum = int(lastnum) + 1
-    for i in range(9):
-        lastnum = lastnum + 1
-        existing_file.write(str(lastnum)+"\n")
+def GCD(G):
+    
+print(GCD(int(input)))
